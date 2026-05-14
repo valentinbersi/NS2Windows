3764e5d1062df91a616794810801be73fc958c81
Valentín Bersi <valentinbersi@gmail.com>
2026-05-14 07:13:02 +0000

    feat ✨: integrated database, Bluetooth adapter setup, and updated Tauri app lifecycle with commands and shared application state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,30 @@
+use crate::commands::connections::{connect_controller, get_connections, remove_connection};
+use crate::commands::profiles::{
+    delete_profile, find_profile_by_name, profile_names, save_profile,
+};
+use crate::communication::communicator::BluetoothCommunicator;
+use crate::connection::connector::BluetoothConnector;
+use crate::repositories::profile_repository::ProfileRepository;
+use crate::state::AppState;
+use btleplug::api::Manager as BManager;
+use maplit::hashmap;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::{Database, DbErr};
+use tauri::Manager as TManager;
+use tokio::sync::RwLock;
+
+pub mod commands;
+pub mod communication;
+pub mod connection;
+pub mod data;
+pub mod decode;
+pub mod dtos;
+pub mod encode;
+pub mod entities;
+pub mod profiles;
+pub mod repositories;
+pub mod state;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -5,10 +32,42 @@ fn greet(name: &str) -> String {
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub fn run() -> tauri::Result<()> {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            save_profile,
+            delete_profile,
+            find_profile_by_name,
+            profile_names,
+            connect_controller,
+            get_connections,
+            remove_connection,
+        ])
+        .setup(|app| {
+            let db = tauri::async_runtime::block_on(async {
+                let db = Database::connect("sqlite://app.db").await?;
+                Migrator::up(&db, None).await?;
+                Ok::<sea_orm::DatabaseConnection, DbErr>(db)
+            })?;
+
+            let adapter = tauri::async_runtime::block_on(async {
+                let manager = btleplug::platform::Manager::new().await?;
+                let adapters = manager.adapters().await?;
+                Ok::<Option<btleplug::platform::Adapter>, btleplug::Error>(
+                    adapters.first().cloned(),
+                )
+            })?
+            .ok_or("No Bluetooth adapters found")?;
+
+            app.manage(AppState {
+                profile_repository: ProfileRepository::new(db),
+                connector: BluetoothConnector::new(adapter),
+                communicator: BluetoothCommunicator,
+                connected_controllers: RwLock::new(hashmap!()),
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+fn main() -> tauri::Result<()> {
     s2windows_lib::run()
 }
