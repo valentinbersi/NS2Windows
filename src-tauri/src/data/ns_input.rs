8f262a19e26840e99407a9fe646377bf2211e4bd
Valentín Bersi <valentinbersi@gmail.com>
2026-05-17 06:39:44 +0000

    refactor: ♻️ removed unused `to_string` and `Display` implementations from `Output` and `NsInput` enums for cleanup

--- src-tauri/src/data/ns_input.rs
+++ src-tauri/src/data/ns_input.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::fmt::{Display, Formatter};
+use std::fmt::Display;
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub enum NsInput {
@@ -60,53 +60,4 @@ pub enum NsInput {
     GyroRollRight,
     GyroYawLeft,
     GyroYawRight,
-}
-
-impl Display for NsInput {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(match self {
-            NsInput::B => "B",
-            NsInput::A => "A",
-            NsInput::Y => "Y",
-            NsInput::X => "X",
-            NsInput::Home => "Home",
-            NsInput::Capture => "Capture",
-            NsInput::R => "R",
-            NsInput::Tr => "TR",
-            NsInput::Zr => "ZR",
-            NsInput::Sr => "SR",
-            NsInput::Gr => "GR",
-            NsInput::L => "L",
-            NsInput::Tl => "TL",
-            NsInput::Zl => "ZL",
-            NsInput::Sl => "SL",
-            NsInput::Gl => "GL",
-            NsInput::Plus => "Plus",
-            NsInput::Minus => "Minus",
-            NsInput::Down => "Down",
-            NsInput::Left => "Left",
-            NsInput::Right => "Right",
-            NsInput::Up => "Up",
-            NsInput::LeftXMinus => "Left X-",
-            NsInput::LeftXPlus => "Left X+",
-            NsInput::LeftYMinus => "Left Y-",
-            NsInput::LeftYPlus => "Left Y+",
-            NsInput::RightXMinus => "Right X-",
-            NsInput::RightXPlus => "Right X+",
-            NsInput::RightYMinus => "Right Y-",
-            NsInput::RightYPlus => "Right Y+",
-            NsInput::AccelUp => "Accel Up",
-            NsInput::AccelDown => "Accel Down",
-            NsInput::AccelLeft => "Accel Left",
-            NsInput::AccelRight => "Accel Right",
-            NsInput::AccelForward => "Accel Forward",
-            NsInput::AccelBackward => "Accel Backward",
-            NsInput::GyroPitchUp => "Gyro Pitch Up",
-            NsInput::GyroPitchDown => "Gyro Pitch Down",
-            NsInput::GyroRollLeft => "Gyro Roll Left",
-            NsInput::GyroRollRight => "Gyro Roll Right",
-            NsInput::GyroYawLeft => "Gyro Yaw Left",
-            NsInput::GyroYawRight => "Gyro Yaw Right",
-        })
-    }
 }
--- src-tauri/src/data/output.rs
+++ src-tauri/src/data/output.rs
@@ -1,4 +1,3 @@
-use crate::data::profile_kind::ProfileKind;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize, Deserialize)]
@@ -33,11 +32,13 @@ pub enum Output {
     Right,
     Up,
 
-    // ----------- Joystick input -----------
+    // ----------- Left joystick output -----------
     LeftXMinus,
     LeftXPlus,
     LeftYMinus,
     LeftYPlus,
+
+    // ----------- Right joystick output -----------
     RightXMinus,
     RightXPlus,
     RightYMinus,
@@ -56,70 +57,4 @@ pub enum Output {
     GyroRollRight,
     GyroYawLeft,
     GyroYawRight,
-}
-
-impl Output {
-    pub fn to_string(&self, profile_kind: ProfileKind) -> Option<&'static str> {
-        Some(match self {
-            Output::Down => "Down",
-            Output::Left => "Left",
-            Output::Right => "Right",
-            Output::Up => "Up",
-            Output::LeftXMinus => "Left X-",
-            Output::LeftXPlus => "Left X+",
-            Output::LeftYMinus => "Left Y-",
-            Output::LeftYPlus => "Left Y+",
-            Output::RightXMinus => "Right X-",
-            Output::RightXPlus => "Right X+",
-            Output::RightYMinus => "Right Y-",
-            Output::RightYPlus => "Right Y+",
-            _ => match profile_kind {
-                ProfileKind::Ps4 => match self {
-                    Output::CrossA => "Cross",
-                    Output::CircleB => "Circle",
-                    Output::SquareX => "Square",
-                    Output::TriangleY => "Triangle",
-                    Output::PsGuide => "Ps",
-                    Output::Share => "Share",
-                    Output::R1Rb => "R1",
-                    Output::R2Rt => "R2",
-                    Output::R3Rs => "R3",
-                    Output::L1Lb => "L1",
-                    Output::L2Lt => "L2",
-                    Output::L3Ls => "L3",
-                    Output::OptionsStart => "Options",
-                    Output::TouchpadBack => "Touchpad",
-                    Output::AccelUp => "Accel Up",
-                    Output::AccelDown => "Accel Down",
-                    Output::AccelLeft => "Accel Left",
-                    Output::AccelRight => "Accel Right",
-                    Output::AccelForward => "Accel Forward",
-                    Output::AccelBackward => "Accel Backward",
-                    Output::GyroPitchUp => "Gyro Pitch Up",
-                    Output::GyroPitchDown => "Gyro Pitch Down",
-                    Output::GyroRollLeft => "Gyro Roll Left",
-                    Output::GyroRollRight => "Gyro Roll Right",
-                    Output::GyroYawLeft => "Gyro Yaw Left",
-                    Output::GyroYawRight => "Gyro Yaw Right",
-                    _ => panic!(),
-                },
-                ProfileKind::Xbox360 => match self {
-                    Output::CrossA => "A",
-                    Output::CircleB => "B",
-                    Output::SquareX => "X",
-                    Output::TriangleY => "Y",
-                    Output::PsGuide => "Guide",
-                    Output::R1Rb => "Lb",
-                    Output::R2Rt => "Rt",
-                    Output::R3Rs => "Rs",
-                    Output::L1Lb => "Lb",
-                    Output::L2Lt => "Lt",
-                    Output::L3Ls => "Ls",
-                    Output::OptionsStart => "Start",
-                    Output::TouchpadBack => "Back",
-                    _ => panic!(),
-                },
-            },
-        })
-    }
 }
