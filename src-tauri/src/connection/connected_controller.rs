3bf489d57fd9738e1e1ac4d6415c8a0335bab86f
Valentín Bersi <valentinbersi@gmail.com>
2026-05-23 04:55:11 +0000

    feat: ✨ added disconnect method to structs in connected_controller.rs

--- src-tauri/src/connection/connected_controller.rs
+++ src-tauri/src/connection/connected_controller.rs
@@ -1,6 +1,6 @@
 use crate::connection::joy_con_side::JoyConSide;
 use crate::connection::motion_source::MotionSource;
-use btleplug::api::Characteristic;
+use btleplug::api::{Characteristic, Peripheral as PeripheralApi};
 use btleplug::platform::Peripheral;
 use std::sync::Arc;
 
@@ -11,10 +11,22 @@ pub struct ConnectedDevice {
     pub write_char: Arc<Characteristic>,
 }
 
+impl ConnectedDevice {
+    pub async fn disconnect(&self) -> btleplug::Result<()> {
+        self.peripheral.disconnect().await
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct ConnectedSingleJoyCon {
     pub device: ConnectedDevice,
     pub joy_con_side: JoyConSide,
+}
+
+impl ConnectedSingleJoyCon {
+    pub async fn disconnect(&self) -> btleplug::Result<()> {
+        self.device.disconnect().await
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -24,14 +36,33 @@ pub struct ConnectedDualJoyCon {
     pub motion_source: MotionSource,
 }
 
+impl ConnectedDualJoyCon {
+    pub async fn disconnect(&self) -> btleplug::Result<()> {
+        self.left.disconnect().await?;
+        self.right.disconnect().await
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct ConnectedProController {
     pub device: ConnectedDevice,
 }
 
+impl ConnectedProController {
+    pub async fn disconnect(&self) -> btleplug::Result<()> {
+        self.device.disconnect().await
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct ConnectedNsoGcController {
     pub device: ConnectedDevice,
+}
+
+impl ConnectedNsoGcController {
+    pub async fn disconnect(&self) -> btleplug::Result<()> {
+        self.device.disconnect().await
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -40,4 +71,15 @@ pub enum ConnectedController {
     DualJoyCon(ConnectedDualJoyCon),
     ProController(ConnectedProController),
     NsoGcController(ConnectedNsoGcController),
+}
+
+impl ConnectedController {
+    pub async fn disconnect(&self) -> btleplug::Result<()> {
+        match self {
+            ConnectedController::SingleJoyCon(joy_con) => joy_con.disconnect().await,
+            ConnectedController::DualJoyCon(joy_cons) => joy_cons.disconnect().await,
+            ConnectedController::ProController(controller) => controller.disconnect().await,
+            ConnectedController::NsoGcController(controller) => controller.disconnect().await,
+        }
+    }
 }
