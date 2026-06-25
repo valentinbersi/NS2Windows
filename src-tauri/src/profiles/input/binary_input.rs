544de79441c9c81cc14ec1d6533c3ff46ed8fc0c
Valentín Bersi <valentinbersi@gmail.com>
2026-06-25 20:30:07 +0000

    feat: ✨ added BinaryInput struct

---
+++ src-tauri/src/profiles/input/binary_input.rs
@@ -0,0 +1,31 @@
+use crate::data::input_data::InputData;
+use crate::profiles::input::binary_operator::BinaryOperator;
+use crate::profiles::input::input::Input;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize, Deserialize)]
+pub struct BinaryInput {
+    left: Input,
+    right: Input,
+    operator: BinaryOperator,
+}
+
+impl BinaryInput {
+    pub fn new(left: Input, right: Input, operator: BinaryOperator) -> Self {
+        Self {
+            left,
+            right,
+            operator,
+        }
+    }
+
+    pub fn evaluate(&self, input: &InputData) -> f32 {
+        let left = self.left.evaluate(input);
+        let right = self.right.evaluate(input);
+
+        match self.operator {
+            BinaryOperator::And => left.min(right),
+            BinaryOperator::Or => left.max(right),
+        }
+    }
+}
