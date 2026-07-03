e53b33ea06701c44b234a994510ad21c67aa55a7
Valentín Bersi <valentinbersi@gmail.com>
2026-07-03 22:27:17 +0000

    refactor: 🔨 splitted input poolers into unique listener and common listener

--- src-tauri/src/state/emulated_controller_task.rs
+++ src-tauri/src/state/emulated_controller_task.rs
@@ -2,36 +2,45 @@ use tauri::async_runtime::JoinHandle;
 
 pub enum EmulatedControllerTask {
     SingleController {
-        input_pooler: JoinHandle<()>,
+        unique_input_listener: JoinHandle<()>,
+        common_input_listener: JoinHandle<()>,
         output_emulator: JoinHandle<()>,
     },
     DualJoyCon {
-        left: JoinHandle<()>,
-        right: JoinHandle<()>,
-        output: JoinHandle<()>,
+        left_unique_input_listener: JoinHandle<()>,
+        left_common_input_listener: JoinHandle<()>,
+        right_unique_input_listener: JoinHandle<()>,
+        right_common_input_listener: JoinHandle<()>,
+        output_emulator: JoinHandle<()>,
     },
 }
 
 impl EmulatedControllerTask {
     pub fn new_single_controller(
-        input_pooler: JoinHandle<()>,
+        unique_input_listener: JoinHandle<()>,
+        common_input_listener: JoinHandle<()>,
         output_emulator: JoinHandle<()>,
     ) -> Self {
         Self::SingleController {
-            input_pooler,
+            unique_input_listener,
+            common_input_listener,
             output_emulator,
         }
     }
 
     pub fn new_dual_joy_con(
-        left: JoinHandle<()>,
-        right: JoinHandle<()>,
-        output: JoinHandle<()>,
+        left_unique_input_listener: JoinHandle<()>,
+        left_common_input_listener: JoinHandle<()>,
+        right_unique_input_listener: JoinHandle<()>,
+        right_common_input_listener: JoinHandle<()>,
+        output_emulator: JoinHandle<()>,
     ) -> Self {
         Self::DualJoyCon {
-            left,
-            right,
-            output,
+            left_unique_input_listener,
+            left_common_input_listener,
+            right_unique_input_listener,
+            right_common_input_listener,
+            output_emulator,
         }
     }
 }
@@ -40,21 +49,27 @@ impl Drop for EmulatedControllerTask {
     fn drop(&mut self) {
         match self {
             EmulatedControllerTask::SingleController {
-                input_pooler,
+                unique_input_listener,
+                common_input_listener,
                 output_emulator,
             } => {
                 output_emulator.abort();
-                input_pooler.abort();
+                common_input_listener.abort();
+                unique_input_listener.abort();
             }
 
             EmulatedControllerTask::DualJoyCon {
-                left,
-                right,
-                output,
+                left_unique_input_listener,
+                left_common_input_listener,
+                right_unique_input_listener,
+                right_common_input_listener,
+                output_emulator,
             } => {
-                left.abort();
-                right.abort();
-                output.abort();
+                output_emulator.abort();
+                left_unique_input_listener.abort();
+                left_common_input_listener.abort();
+                right_unique_input_listener.abort();
+                right_common_input_listener.abort();
             }
         }
     }
