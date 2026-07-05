70bf9f20583628f047fe5d296e1ab716eb827b52
Valentín Bersi <valentinbersi@gmail.com>
2026-07-05 06:57:17 +0000

    feat: ✨ added rumble_output and combined_output characteristic discovering

--- src-tauri/src/connection/connector.rs
+++ src-tauri/src/connection/connector.rs
@@ -8,11 +8,10 @@ use btleplug::platform::{Adapter, Peripheral};
 use futures::StreamExt;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
-use std::str::FromStr;
 use std::time::Duration;
 use tokio::time::error::Elapsed;
 use tokio::time::timeout;
-use uuid::Uuid;
+use uuid::{Uuid, uuid};
 
 const JOY_CON_MANUFACTURER_ID: u16 = 1363; // Nintendo
 const JOY_CON_MANUFACTURER_PREFIX: [u8; 4] = [0x01, 0x00, 0x03, 0x7E];
@@ -53,15 +52,11 @@ impl From<Elapsed> for ConnectorError {
     }
 }
 
-const COMMON_INPUT_UUID: &str = "ab7de9be-89fe-49ad-828f-118f09df7fd2";
-const LEFT_JOY_CON_INPUT_UUID: &str = "cc1bbbb5-7354-4d32-a716-a81cb241a32a";
-const RIGHT_JOY_CON_INPUT_UUID: &str = "d5a9e01e-2ffc-4cca-b20c-8b67142bf442";
-const PRO_CONTROLLER_INPUT_UUID: &str = "7492866c-ec3e-4619-8258-32755ffcc0f8";
-const NSO_GC_CONTROLLER_INPUT_UUID: &str = "8261cba1-9435-420c-84d6-f0c75a2c8e4d";
-
 struct Characteristics {
     input: Characteristic,
     output: Characteristic,
+    rumble_output: Characteristic,
+    combined_output: Characteristic,
 }
 
 impl BluetoothConnector {
@@ -119,69 +114,91 @@ impl BluetoothConnector {
         .and_then(|value| value.map_err(Into::into))
     }
 
+    const OUTPUT_UUID: Uuid = uuid!("649d4ac9-8eb7-4e6c-af44-1ea54fe5f005");
+
+    const COMMON_INPUT_UUID: Uuid = uuid!("ab7de9be-89fe-49ad-828f-118f09df7fd2");
+    const LEFT_JOY_CON_INPUT_UUID: Uuid = uuid!("cc1bbbb5-7354-4d32-a716-a81cb241a32a");
+    const RIGHT_JOY_CON_INPUT_UUID: Uuid = uuid!("d5a9e01e-2ffc-4cca-b20c-8b67142bf442");
+    const PRO_CONTROLLER_INPUT_UUID: Uuid = uuid!("7492866c-ec3e-4619-8258-32755ffcc0f8");
+    const NSO_GC_CONTROLLER_INPUT_UUID: Uuid = uuid!("8261cba1-9435-420c-84d6-f0c75a2c8e4d");
+
+    const LEFT_JOY_CON_RUMBLE_OUTPUT_UUID: Uuid = uuid!("289326cb-a471-485d-a8f4-240c14f18241");
+    const RIGHT_JOY_CON_RUMBLE_OUTPUT_UUID: Uuid = uuid!("fa19b0fb-cd1f-46a7-84a1-bbb09e00c149");
+    const PRO_CONTROLLER_RUMBLE_OUTPUT_UUID: Uuid = uuid!("cc483f51-9258-427d-a939-630c31f72b05");
+    const NSO_GC_CONTROLLER_RUMBLE_OUTPUT_UUID: Uuid =
+        uuid!("3f8fb670-ab25-45bf-b540-38c72834d064");
+
+    const LEFT_JOY_CON_COMBINED_OUTPUT_UUID: Uuid = uuid!("ce49a830-dced-48ae-931e-c8cf88aadbea");
+    const RIGHT_JOY_CON_COMBINED_OUTPUT_UUID: Uuid = uuid!("65a724b3-f1e7-4a61-8078-a342376b27ff");
+    const PRO_CONTROLLER_COMBINED_OUTPUT_UUID: Uuid = uuid!("3dacbc7e-6955-40b5-8eaf-6f9809e8b379");
+    const NSO_GC_CONTROLLER_COMBINED_OUTPUT_UUID: Uuid =
+        uuid!("af95885e-44b3-4a24-9cf0-483cc129469a");
+
     fn discover_characteristics(
         &self,
         controller: &Peripheral,
     ) -> Result<(NsControllerKind, Characteristics), ConnectorError> {
         let mut input = None;
-        let mut controller_kind = None;
+        let mut kind = None;
         let mut output = None;
-
-        let common_input_uuid = Uuid::from_str(COMMON_INPUT_UUID).unwrap();
-        let left_joy_con_input_uuid = Uuid::from_str(LEFT_JOY_CON_INPUT_UUID).unwrap();
-        let right_joy_con_input_uuid = Uuid::from_str(RIGHT_JOY_CON_INPUT_UUID).unwrap();
-        let pro_controller_input_uuid = Uuid::from_str(PRO_CONTROLLER_INPUT_UUID).unwrap();
-        let nso_gc_controller_input_uuid = Uuid::from_str(NSO_GC_CONTROLLER_INPUT_UUID).unwrap();
-        let output_uuid = Uuid::from_str("649d4ac9-8eb7-4e6c-af44-1ea54fe5f005").unwrap();
+        let mut rumble_output = None;
+        let mut combined_output = None;
 
         for characteristic in controller.characteristics() {
-            if input.is_some() && controller_kind.is_some() && output.is_some() {
+            if input.is_some()
+                && kind.is_some()
+                && output.is_some()
+                && rumble_output.is_some()
+                && combined_output.is_some()
+            {
                 break;
             }
 
-            let uuid = characteristic.uuid;
+            match characteristic.uuid {
+                Self::OUTPUT_UUID => output = Some(characteristic),
 
-            // Get the common input characteristic
-            if uuid == common_input_uuid {
-                input = Some(characteristic);
-                continue;
-            }
+                Self::COMMON_INPUT_UUID => input = Some(characteristic),
 
-            // Get the output characteristic
-            if uuid == output_uuid {
-                output = Some(characteristic);
-                continue;
-            }
+                Self::LEFT_JOY_CON_INPUT_UUID => kind = Some(NsControllerKind::LeftJoyCon),
+                Self::RIGHT_JOY_CON_INPUT_UUID => kind = Some(NsControllerKind::RightJoyCon),
+                Self::PRO_CONTROLLER_INPUT_UUID => kind = Some(NsControllerKind::ProController),
+                Self::NSO_GC_CONTROLLER_INPUT_UUID => {
+                    kind = Some(NsControllerKind::NsoGcController)
+                }
 
-            // Get the left joy con unique characteristic
-            if uuid == left_joy_con_input_uuid {
-                controller_kind = Some(NsControllerKind::LeftJoyCon);
-                continue;
-            }
+                Self::LEFT_JOY_CON_RUMBLE_OUTPUT_UUID
+                | Self::RIGHT_JOY_CON_RUMBLE_OUTPUT_UUID
+                | Self::PRO_CONTROLLER_RUMBLE_OUTPUT_UUID
+                | Self::NSO_GC_CONTROLLER_RUMBLE_OUTPUT_UUID => {
+                    rumble_output = Some(characteristic)
+                }
 
-            // Get the right joy con unique characteristic
-            if uuid == right_joy_con_input_uuid {
-                controller_kind = Some(NsControllerKind::RightJoyCon);
-                continue;
-            }
+                Self::LEFT_JOY_CON_COMBINED_OUTPUT_UUID
+                | Self::RIGHT_JOY_CON_COMBINED_OUTPUT_UUID
+                | Self::PRO_CONTROLLER_COMBINED_OUTPUT_UUID
+                | Self::NSO_GC_CONTROLLER_COMBINED_OUTPUT_UUID => {
+                    combined_output = Some(characteristic)
+                }
 
-            // Get the pro controller unique characteristic
-            if uuid == pro_controller_input_uuid {
-                controller_kind = Some(NsControllerKind::ProController);
-                continue;
-            }
-
-            // Get the nso gc controller unique characteristic
-            if uuid == nso_gc_controller_input_uuid {
-                controller_kind = Some(NsControllerKind::NsoGcController);
+                _ => {}
             }
         }
 
         let input = input.ok_or_else(|| ConnectorError::ConnectionError)?;
-        let controller_kind = controller_kind.ok_or_else(|| ConnectorError::ConnectionError)?;
+        let controller_kind = kind.ok_or_else(|| ConnectorError::ConnectionError)?;
         let output = output.ok_or_else(|| ConnectorError::ConnectionError)?;
+        let rumble_output = rumble_output.ok_or_else(|| ConnectorError::ConnectionError)?;
+        let combined_output = combined_output.ok_or_else(|| ConnectorError::ConnectionError)?;
 
-        Ok((controller_kind, Characteristics { input, output }))
+        Ok((
+            controller_kind,
+            Characteristics {
+                input,
+                output,
+                rumble_output,
+                combined_output,
+            },
+        ))
     }
 
     pub async fn wait_for_controller(&self) -> Result<ConnectedController, ConnectorError> {
@@ -207,6 +224,8 @@ impl BluetoothConnector {
             controller,
             characteristics.input,
             characteristics.output,
+            characteristics.rumble_output,
+            characteristics.combined_output,
             controller_kind,
         );
 
